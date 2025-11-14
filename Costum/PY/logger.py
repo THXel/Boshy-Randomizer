b7a1b84320a4e06c2ed6eba0441a8e7645dd18c9
THXel <thxel@ragres.de>
2025-11-14 19:32:59 +0100

    Delete Costum/PY directory

--- Costum/PY/logger.py
+++
@@ -1,9 +0,0 @@
-import time
-from .config import debug_log
-
-def log(msg):
-    ts = time.strftime("%Y-%m-%d %H:%M:%S")
-    line = f"[{ts}] {msg}"
-    print(line)
-    with open(debug_log, "a", encoding="utf-8") as f:
-        f.write(line + "\n")
